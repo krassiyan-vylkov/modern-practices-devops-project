ed783b3f82a1ee64288edfcf55d94e9c4eeacb21
Krassiyan Vylkov <krasenvola@gmail.com>
2026-01-02 11:19:16 +0200

    Added SonarQube to pipeline

--- app/init_db.py
+++ app/init_db.py
@@ -9,12 +9,21 @@ operators = [
     Operator(name="Exusiai", attack=650, damage_type="physical"),
     Operator(name="SilverAsh", attack=900, damage_type="physical"),
     Operator(name="Eyjafjalla", attack=800, damage_type="arts"),
+    Operator(name="Surtr", attack=2000, damage_type="arts"),
+    Operator(name="Mlynar", attack=1800, damage_type="physical"),
+    Operator(name="Mons3r", attack=1200, damage_type="true"),
+    Operator(name="Saga", attack=1000, damage_type="physical"),
+    Operator(name="Bubble", attack=300, damage_type="physical"),
 ]
 
 bosses = [
     Boss(name="Patriot", defense=1200, resistance=50),
     Boss(name="FrostNova", defense=600, resistance=70),
     Boss(name="Crownslayer", defense=400, resistance=20),
+    Boss(name="Last Steam Knight", defense=2000, resistance=10),
+    Boss(name="Minimalist", defense=200, resistance=40),
+    Boss(name="Pompeii", defense=200, resistance=80),
+    Boss(name="Mechanist", defense=1400, resistance=50),
 ]
 
 db.add_all(operators + bosses)
