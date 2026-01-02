8d3b6ba3332e4b729903cb190ff8c3fd23b108b0
Krassiyan Vylkov <krasenvola@gmail.com>
2026-01-02 15:49:15 +0200

    Added k8s deployments

--- app/database.py
+++ app/database.py
@@ -1,14 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Path to database.db in project root
-DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "database.db")
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://arknights:arknights@postgres:5432/arknights"
 )
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
